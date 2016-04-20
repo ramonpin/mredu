002f4c8e964218a34f43e2c93cc4d92297d475c8
Ramón Pin <ramon.pin@gmail.com>
2016-04-21 00:10:48 +0200

    Translated output

--- example1.py
+++ example1.py
@@ -2,13 +2,13 @@ from mredu import simul
 
 l = [('a', 1), ('b', 2), ('a', 3), ('c', 9), ('b', 6)]                                
 
-print('Suma de los valores por clave...')
+print('Sum all values by key...')
 print('-' * 50)
 process = simul.map_red(l, reducer=lambda k,v: (k, sum(v))) 
 simul.run(process, sep=',')
 
 print()
-print('Despliege de la clave por el valor...')
+print('Unrolls key by value...')
 print('-' * 50)
 process = simul.map_red(l, mapper=lambda k, v: [(k, 1) for _ in range(v)])
 simul.run(process, sep=',')
