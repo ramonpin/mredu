e0e123bd6b1705890504b924197dfe9f70d82d17
Ramón Pin <ramon.pin@gmail.com>
2016-04-21 22:10:32 +0200

    Added example of reading from kv file.

---
+++ example4.py
@@ -0,0 +1,15 @@
+from mredu.simul import map_red, input_kv_file, run
+from re import split
+
+
+def mymap(k, v):
+    return v, k
+
+
+def myred(k, vs):
+    return k, len(vs)
+
+
+if __name__ == '__main__':
+    process = map_red(input_kv_file('data/palabras.txt'), mymap, myred)
+    run(process)
