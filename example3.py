b0ac43a680552e3c6711b2de62cf3b3b4b06f149
Ramón Pin <ramon.pin@gmail.com>
2016-04-21 22:00:10 +0200

    Remove stopwords from histogram

--- example3.py
+++ example3.py
@@ -1,10 +1,17 @@
 from mredu.simul import map_red, input_file, run
 from re import split
 
+stopwords = ['así', 'para', 'sus', 'una', 'ni', 'porque', 'sin', 'tan', 'al',
+             'si', 'me', 'un', 'más', 'es', 'del', 'lo', 'las', 'le', 'mas',
+             'por', 'su', 'con', 'los', 'se', 'no', 'en', 'el', 'la', 'a', 'y',
+             'de', 'que', 'muy', 'qué', 'como', 'mi', 'o', 'aquel', 'ya',
+             'pues', 'cuando', 'cual', 'pero', 'este', 'esto', 'aquí',
+             'aquella', 'aquello']
+
 
 def mymap(_, v):
     words = list(filter(lambda s: s != '', split(r'\W', v)))
-    return [(word.lower(), 1) for word in words]
+    return [(word.lower(), 1) for word in words if word not in stopwords]
 
 
 def myred(k, vs):
