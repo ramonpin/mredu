bd2bf281258b9f7f54a326359d82ba5f4ea6bc88
ramonpin <ramon.pin@gmail.com>
2025-08-20 13:21:57 +0200

    feat: Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
-setup(name='mredu',
-      version='0.4.1',
-      description='A simple simulator of a system which implements map/reduce paradigm.',
-      url='http://github.com/ramonpin/mredu',
-      author='ramonpin',
-      author_email='ramon.pin@gmail.com',
-      license='Apache-2',
-      packages=['mredu'],
-      install_requires=[
-        'toolz>=0.12.0',
-        'rich',
-      ],
-      zip_safe=False)
+setup(
+    name="mredu",
+    version="1.0.0",
+    description="A simple simulator of a system which implements map/reduce paradigm.",
+    url="http://github.com/ramonpin/mredu",
+    author="ramonpin",
+    author_email="ramon.pin@gmail.com",
+    license="Apache-2",
+    packages=["mredu"],
+    install_requires=[
+        "toolz>=0.12.0",
+        "rich",
+    ],
+    zip_safe=False,
+)
