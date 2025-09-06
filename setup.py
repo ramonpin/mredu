23c13933929a60c702eec3a998bbf028f8612ee6
ramonpin <ramon.pin@gmail.com>
2025-09-06 20:02:29 +0200

    feat: Add long description to package

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="mredu",
-    version="1.0.0",
-    description="A simple simulator of a system which implements map/reduce paradigm.",
+    version="1.0.1",
     url="http://github.com/ramonpin/mredu",
     author="ramonpin",
     author_email="ramon.pin@gmail.com",
+    description="A simple simulator of a system which implements map/reduce paradigm.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="Apache-2",
     packages=["mredu"],
     install_requires=[
